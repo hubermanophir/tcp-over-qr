5f3885ffb6304731505704db5dac37544c6b949c
hubermanophir <ofirhuberman@gmail.com>
2025-10-16 22:15:59 +0300

    added initial context for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { createContext, useState } from "react";
+
+type SenderContextType = {
+  isSender: boolean;
+  setIsSender: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const SenderContext = createContext<SenderContextType>({
+  isSender: false,
+  setIsSender: (_value: any) => {},
+});
 function App() {
+  const [isSender, setIsSender] = useState(false);
+
   return (
-    <>
-      <div>
-        test text
-        <div className="text-green-500 bg-green-100">Test</div>
-      </div>
-    </>
+    <SenderContext.Provider value={{ isSender, setIsSender }}>
+      <>
+        <div>
+          test text
+          <div className="text-green-500 bg-green-100">Test</div>
+        </div>
+      </>
+    </SenderContext.Provider>
   );
 }
 
