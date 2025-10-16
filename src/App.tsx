a748996bcc71f25bcb14a1773e52b2653f35146c
hubermanophir <ofirhuberman@gmail.com>
2025-10-16 22:06:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        test text
+        <div className="text-green-500 bg-green-100">Test</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
