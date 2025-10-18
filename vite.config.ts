d82b53aea0cbb5a83c0ca0c5f51539370224a77e
hubermanophir <ofirhuberman@gmail.com>
2025-10-18 13:46:17 +0300

    added self signed ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite"; // Import tailwindcss plugin
-
+import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
